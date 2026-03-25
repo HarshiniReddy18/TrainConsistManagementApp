b74492164b56424728b3eb6d603630b0ff282b24
Harshini Reddy <vharshinereddy@gmail.com>
2026-03-25 14:13:03 +0530

    Created GoodsBogie class extending Bogie

---
+++ com/trainconsist/model/GoodsBogie.java
@@ -0,0 +1,11 @@
+package com.trainconsist.model;
+
+public class GoodsBogie extends Bogie {
+    // Define properties, constructors, methods specific to GoodsBogie
+    
+    public GoodsBogie() {
+        // Initialize GoodsBogie specific attributes
+    }
+    
+    // Additional methods for GoodsBogie can be added here
+}
